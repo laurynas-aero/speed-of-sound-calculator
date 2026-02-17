255e631dc3232e8e5ded76a44219b9500a1c017d
Spiller362 <laurynasladd@yahoo.com>
2026-02-17 17:00:57 +0000

    Split away calculation function

--- SpeedOfSound_Calculator.py
+++ SpeedOfSound_Calculator.py
@@ -1,9 +1,4 @@
-import aerosandbox
-
-#  Constant Variable Assignment
-RATIO_OF_SPECIFIC_HEATS = 1.4
-SPECIFIC_HEAT_SI = 287
-SPECIFIC_HEAT_ENGLISH = 1716
+from calculator import compute_speed_of_sound
 
 def get_float_or_unknown(prompt):
     value = input(prompt)
@@ -27,54 +22,9 @@ def main():
     temperature = get_float_or_unknown('What is the temperature?')
     speed_of_sound = get_float_or_unknown('What is the speed of sound?')
 
-    #   Finding Known
-    if alt_m is not None:
-        known = alt_m
-    elif temperature is not None:
-        known = temperature
-    elif speed_of_sound is not None:
-        known = speed_of_sound
-    else:
-        print("ERROR: No valid input provided.")
-        return
-
-
-    #   Constant in Units and Altitude
-    if units.lower() == 'english':
-        constant = RATIO_OF_SPECIFIC_HEATS*SPECIFIC_HEAT_ENGLISH
-        speed = 'ft/s'
-        distance = 'ft'
-        temp = 'R'
-        if alt_m is not None:
-            alt_m = known*0.3048
-
-    elif units.lower() == 'si':
-        constant = RATIO_OF_SPECIFIC_HEATS*SPECIFIC_HEAT_SI
-        speed = 'm/s'
-        distance = 'm'
-        temp = 'K'
-        
-        if alt_m is not None:
-            alt_m = known
-
-
-    if alt_m is not None:
-        atm = aerosandbox.Atmosphere(altitude=alt_m) 
-        T_K = atm.temperature()
-        T_R = T_K*1.8
-        if units.lower() == 'english':
-            speed_of_sound = (constant*T_R)**0.5
-            temperature = T_R
-        elif units == 'si':
-            speed_of_sound = (constant*T_K)**0.5
-            temperature = T_K
-    #   Speed of Sound Calculation
-    elif temperature is not None:
-        speed_of_sound = (constant*known)**0.5
-    elif speed_of_sound is not None:
-        temperature = ((speed_of_sound)**2)/constant
-    else:
-        "ERROR. Please try again."
+    speed_of_sound, temperature, alt_m, speed, temp, distance = compute_speed_of_sound(
+        units, alt_m, temperature, speed_of_sound
+    )
 
     #   Printing Results
     print(f"The speed of sound is: {speed_of_sound} {speed}")
